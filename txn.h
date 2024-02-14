5c678b78db4878eb15fe76fd04b8c6564ca7f69d
YoichiroTacker <youitiro.tacker@gmail.com>
2024-02-14 18:12:57 +0900

    implement proposed method without formatting

--- txn.h
+++ txn.h
@@ -19,130 +19,124 @@
 #include <sparsehash/dense_hash_map>
 using google::dense_hash_map;
 
-namespace ermia
-{
+namespace ermia {
 
-  // A write-set entry is essentially a pointer to the OID array entry
-  // begin updated. The write-set is naturally de-duplicated: repetitive
-  // updates will leave only one entry by the first update. Dereferencing
-  // the entry pointer results a fat_ptr to the new object.
-  struct write_record_t
-  {
-    fat_ptr *entry;
-    write_record_t(fat_ptr *entry) : entry(entry) {}
-    write_record_t() : entry(nullptr) {}
-    inline Object *get_object() { return (Object *)entry->offset(); }
-  };
+// A write-set entry is essentially a pointer to the OID array entry
+// begin updated. The write-set is naturally de-duplicated: repetitive
+// updates will leave only one entry by the first update. Dereferencing
+// the entry pointer results a fat_ptr to the new object.
+struct write_record_t {
+  fat_ptr *entry;
+  write_record_t(fat_ptr *entry) : entry(entry) {}
+  write_record_t() : entry(nullptr) {}
+  inline Object *get_object() { return (Object *)entry->offset(); }
+};
 
-  struct write_set_t
-  {
-    static const uint32_t kMaxEntries = 256;
-    uint32_t num_entries;
-    write_record_t entries[kMaxEntries];
-    write_set_t() : num_entries(0) {}
-    inline void emplace_back(fat_ptr *oe)
-    {
-      ALWAYS_ASSERT(num_entries < kMaxEntries);
-      new (&entries[num_entries]) write_record_t(oe);
-      ++num_entries;
-      ASSERT(entries[num_entries - 1].entry == oe);
-    }
-    inline uint32_t size() { return num_entries; }
-    inline void clear() { num_entries = 0; }
-    inline write_record_t &operator[](uint32_t idx) { return entries[idx]; }
-  };
+struct write_set_t {
+  static const uint32_t kMaxEntries = 256;
+  uint32_t num_entries;
+  write_record_t entries[kMaxEntries];
+  write_set_t() : num_entries(0) {}
+  inline void emplace_back(fat_ptr *oe) {
+    ALWAYS_ASSERT(num_entries < kMaxEntries);
+    new (&entries[num_entries]) write_record_t(oe);
+    ++num_entries;
+    ASSERT(entries[num_entries - 1].entry == oe);
+  }
+  inline uint32_t size() { return num_entries; }
+  inline void clear() { num_entries = 0; }
+  inline write_record_t &operator[](uint32_t idx) { return entries[idx]; }
+};
 
-  class transaction
-  {
-    friend class ConcurrentMasstreeIndex;
-    friend class sm_oid_mgr;
+class transaction {
+  friend class ConcurrentMasstreeIndex;
+  friend class sm_oid_mgr;
 
-  public:
-    typedef TXN::txn_state txn_state;
+public:
+  typedef TXN::txn_state txn_state;
 
 #if defined(SSN) || defined(SSI) || defined(MVOCC)
-    typedef std::vector<dbtuple *> read_set_t;
+  typedef std::vector<dbtuple *> read_set_t;
 #if defined(TAKADA)
     typedef std::vector<dbtuple *> validated_read_set_t; // 追加
 #endif
 #endif
 
-    enum
-    {
-      // use the low-level scan protocol for checking scan consistency,
-      // instead of keeping track of absent ranges
-      TXN_FLAG_LOW_LEVEL_SCAN = 0x1,
 
-      // true to mark a read-only transaction- if a txn marked read-only
-      // does a write, it is aborted. SSN uses it to implement to safesnap.
-      // No bookeeping is done with SSN if this is enable for a tx.
-      TXN_FLAG_READ_ONLY = 0x2,
+  enum {
+    // use the low-level scan protocol for checking scan consistency,
+    // instead of keeping track of absent ranges
+    TXN_FLAG_LOW_LEVEL_SCAN = 0x1,
 
-      TXN_FLAG_READ_MOSTLY = 0x3,
+    // true to mark a read-only transaction- if a txn marked read-only
+    // does a write, it is aborted. SSN uses it to implement to safesnap.
+    // No bookeeping is done with SSN if this is enable for a tx.
+    TXN_FLAG_READ_ONLY = 0x2,
 
-      // A redo transaction running on a backup server using command logging.
-      TXN_FLAG_CMD_REDO = 0x4,
-    };
+    TXN_FLAG_READ_MOSTLY = 0x3,
 
-    inline bool is_read_mostly() { return flags & TXN_FLAG_READ_MOSTLY; }
-    inline bool is_read_only() { return flags & TXN_FLAG_READ_ONLY; }
+    // A redo transaction running on a backup server using command logging.
+    TXN_FLAG_CMD_REDO = 0x4,
+  };
 
-  protected:
-    inline txn_state state() const { return xc->state; }
+  inline bool is_read_mostly() { return flags & TXN_FLAG_READ_MOSTLY; }
+  inline bool is_read_only() { return flags & TXN_FLAG_READ_ONLY; }
 
-    // the absent set is a mapping from (masstree node -> version_number).
-    typedef dense_hash_map<const ConcurrentMasstree::node_opaque_t *, uint64_t> MasstreeAbsentSet;
-    MasstreeAbsentSet masstree_absent_set;
+protected:
+  inline txn_state state() const { return xc->state; }
 
-  public:
-    transaction(uint64_t flags, str_arena &sa);
-    ~transaction();
-    void initialize_read_write();
+  // the absent set is a mapping from (masstree node -> version_number).
+  typedef dense_hash_map<const ConcurrentMasstree::node_opaque_t *, uint64_t > MasstreeAbsentSet;
+  MasstreeAbsentSet masstree_absent_set;
 
-    inline void ensure_active()
-    {
-      volatile_write(xc->state, TXN::TXN_ACTIVE);
-      ASSERT(state() == TXN::TXN_ACTIVE);
-    }
+ public:
+  transaction(uint64_t flags, str_arena &sa);
+  ~transaction();
+  void initialize_read_write();
 
-    rc_t commit();
+  inline void ensure_active() {
+    volatile_write(xc->state, TXN::TXN_ACTIVE);
+    ASSERT(state() == TXN::TXN_ACTIVE);
+  }
+
+  rc_t commit();
 #ifdef SSN
-    rc_t parallel_ssn_commit();
-    rc_t ssn_read(dbtuple *tuple);
+  rc_t parallel_ssn_commit();
+  rc_t ssn_read(dbtuple *tuple);
 #elif defined SSI
-    rc_t parallel_ssi_commit();
-    rc_t ssi_read(dbtuple *tuple);
+  rc_t parallel_ssi_commit();
+  rc_t ssi_read(dbtuple *tuple);
 #elif defined MVOCC
-    rc_t mvocc_commit();
-    rc_t mvocc_read(dbtuple *tuple);
+  rc_t mvocc_commit();
+  rc_t mvocc_read(dbtuple *tuple);
 #else
-    rc_t si_commit();
+  rc_t si_commit();
 #endif
 
-    bool MasstreeCheckPhantom();
-    void Abort();
+  bool MasstreeCheckPhantom();
+  void Abort();
 
-    OID PrepareInsert(OrderedIndex *index, varstr *value, dbtuple **out_tuple);
-    void FinishInsert(OrderedIndex *index, OID oid, const varstr *key, varstr *value, dbtuple *tuple);
-    bool TryInsertNewTuple(OrderedIndex *index, const varstr *key,
-                           varstr *value, OID *inserted_oid);
+  OID PrepareInsert(OrderedIndex *index, varstr *value, dbtuple **out_tuple);
+  void FinishInsert(OrderedIndex *index, OID oid, const varstr *key, varstr *value, dbtuple *tuple);
+  bool TryInsertNewTuple(OrderedIndex *index, const varstr *key,
+                         varstr *value, OID *inserted_oid);
 
-    rc_t Update(IndexDescriptor *index_desc, OID oid, const varstr *k, varstr *v);
+  rc_t Update(IndexDescriptor *index_desc, OID oid, const varstr *k, varstr *v);
 
-  public:
-    // Reads the contents of tuple into v within this transaction context
-    rc_t DoTupleRead(dbtuple *tuple, varstr *out_v);
+ public:
+  // Reads the contents of tuple into v within this transaction context
+  rc_t DoTupleRead(dbtuple *tuple, varstr *out_v);
 
-    // expected public overrides
+  // expected public overrides
 
-    inline str_arena &string_allocator() { return *sa; }
+  inline str_arena &string_allocator() { return *sa; }
 
 #if defined(SSN) || defined(SSI) || defined(MVOCC)
-    inline read_set_t &GetReadSet()
-    {
-      thread_local read_set_t read_set;
-      return read_set;
-    }
+  inline read_set_t &GetReadSet() {
+    thread_local read_set_t read_set;
+    return read_set;
+  }
+#endif
 
 // 追加
 #if defined(TAKADA)
@@ -165,36 +159,33 @@ namespace ermia
     }
 
 #endif
-#endif
 
-    inline write_set_t &GetWriteSet()
-    {
-      thread_local write_set_t write_set;
-      return write_set;
-    }
 
-    inline void add_to_write_set(fat_ptr *entry)
-    {
+  inline write_set_t &GetWriteSet() {
+    thread_local write_set_t write_set;
+    return write_set;
+  }
+
+  inline void add_to_write_set(fat_ptr *entry) {
 #ifndef NDEBUG
-      auto &write_set = GetWriteSet();
-      for (uint32_t i = 0; i < write_set.size(); ++i)
-      {
-        auto &w = write_set[i];
-        ASSERT(w.entry);
-        ASSERT(w.entry != entry);
-      }
-#endif
-      GetWriteSet().emplace_back(entry);
+    auto &write_set = GetWriteSet();
+    for (uint32_t i = 0; i < write_set.size(); ++i) {
+      auto &w = write_set[i];
+      ASSERT(w.entry);
+      ASSERT(w.entry != entry);
     }
+#endif
+    GetWriteSet().emplace_back(entry);
+  }
 
-    inline TXN::xid_context *GetXIDContext() { return xc; }
+  inline TXN::xid_context *GetXIDContext() { return xc; }
 
-  protected:
-    const uint64_t flags;
-    XID xid;
-    TXN::xid_context *xc;
-    sm_tx_log *log;
-    str_arena *sa;
-  };
+ protected:
+  const uint64_t flags;
+  XID xid;
+  TXN::xid_context *xc;
+  sm_tx_log *log;
+  str_arena *sa;
+};
 
-} // namespace ermia
+}  // namespace ermia
